b31c917e4008ca48123b7b4dd9e5da5eaeeaca3b
Carla Epifanio Vivero <148543863+carlaaev@users.noreply.github.com>
2025-05-09 15:05:03 +0100

    updates

--- svelte-app/svelte.config.js
+++ svelte-app/svelte.config.js
@@ -1,5 +1,12 @@
 import adapter from '@sveltejs/adapter-static';
 
-const config = { kit: { adapter: adapter() } };
+const config = {
+    kit: {
+        adapter: adapter(),
+        paths: {
+            base: '/SveltePortfolio',
+        }
+    }  
+};
 
 export default config;
